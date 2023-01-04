6a595a58f32eb0fdf54357a6df5df88432004d09
Ben Brandon/ <benbrandon97@gmail.com>
2023-01-04 14:44:36 +0300

    Update 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     """
-    Parameters
-    ----------
+    The parameters:
     str : str
         The string from which the position needs to be removed
     n : int
         The position of the string to be deleted
 
+    remove_char_at:
     Deletes the character at the nth position of the str.
     The function:
     1. copies the string.
     2. deletes a position of the passed string.
     3. returns the string without a deleted position
 
-    Returns
-    -------
-    str
+    Returns a str
         The copy of the string without the deleted position
     """
 
