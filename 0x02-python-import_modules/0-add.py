f39027cb6048ace114f8f7543c0ec8cb4e9be775
Ben Brandon/ <benbrandon97@gmail.com>
2023-01-06 08:50:56 +0300

    Files

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from add_0 import add
+
+if __name__ == "__main__":
+    """
+    Prints the result of the addition between two numbers
+
+    """
+    x = 1
+    y = 2
+    print("{:d} + {:d} = {:d}".format(x, y, add(x, y)))
